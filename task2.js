d40d464b93907ed3ceccf42f5794fafb20e197f9
AndreDiPop <104424915+AndreDiPop@users.noreply.github.com>
2022-04-29 18:20:45 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -3,7 +3,7 @@ const secp256k1 = require('secp256k1');
 
 const initVector = crypto.randomBytes(16);
 algo = "aes-256-cbc"
-const msg = process.argv[2]; // сообщение, которое я подпишу
+const msg = process.argv[2];
 const digested = digest(msg);
 console.log(`0) Andre's message:
 message: ${msg}
@@ -17,8 +17,8 @@ function digest(str, algo = 'sha256') {
 
 let privateKey;
 do {
-    privateKey = crypto.randomBytes(32); // тут берутся рандомное значние 32 байтов
-} while (!secp256k1.privateKeyVerify(privateKey)); // они берутся пока не сгенерируется приватный ключ из кривых
+    privateKey = crypto.randomBytes(32); 
+} while (!secp256k1.privateKeyVerify(privateKey));
 
 // получить публичный ключ в каком-то компрессном формате
 const publicKey = secp256k1.publicKeyCreate(privateKey);
@@ -31,10 +31,6 @@ console.log(`2) Andre signed his message digest with his privateKey to get its s
 
 const sigObj = secp256k1.ecdsaSign(digested, privateKey); 
 
-// создаем объект подписи на базе элептической кривой 
-//  и подписываем объект дайджест, который мы получили. Подписываем мы хэш. Мы с помощью приватного ключа
-// зашифровываем хеш сообщения. Ер 1 мь он будет 100 лет шифровать, не знаю почему
-
 const sig = sigObj.signature;
 console.log(" Signature:", Buffer.from(sig).toString('hex'));
 
@@ -43,13 +39,6 @@ const digest_bad = digest("buy");
 console.log(`3) BOb verifyed by 3 elements ("message digest", 'signature', and Andre's "publicKey"):`);
 let verified = secp256k1.ecdsaVerify(sig, digested, publicKey);
 console.log(' verified:', verified);
-
-
-// Боб получает сообщение hi, считаешь от него дайджест, потом он берет подпись, которую Андре ему переслал
-// и
-
-
-// далее зашифрую и расшифрую
 
 const cipher = crypto.createCipheriv(algo, privateKey, initVector);
 
