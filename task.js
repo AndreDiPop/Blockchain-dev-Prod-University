11bd2e09f315e50a715fe8cde7bc8d826d3045e1
Andre <104424915+AndreDiPop@users.noreply.github.com>
2022-05-02 17:26:58 +0300

    Delete task.js

--- task.js
+++
@@ -1,4 +0,0 @@
-var crypto = require('crypto');
-var str = 'SLOZNA! SLOZNA!!';
-var result = crypto.createHash('sha256').update(str).digest('hex');
-console.log('str: ' + str + ", result: " + result);
